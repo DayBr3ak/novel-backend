242a880a0cbfe0d623a4b4f268e1d7e99c4fa8e5
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2018-08-25 16:17:16 +0200

    init

---
+++ index.js
@@ -0,0 +1,32 @@
+"use strict";
+
+const compress = require("compression");
+const cors = require("cors");
+const helmet = require("helmet");
+const feathers = require("@feathersjs/feathers");
+const express = require("@feathersjs/express");
+
+const app = express(feathers());
+app.use(cors());
+app.use(helmet());
+app.use(compress());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use("/", express.static(app.get("public")));
+
+// Set up Plugins and providers
+app.configure(express.rest());
+
+// app.configure(services);
+
+// Configure a middleware for 404s and the error handler
+app.use(express.notFound());
+
+const port = 80;
+const server = app.listen(port);
+
+process.on("unhandledRejection", (reason, p) =>
+  console.error("Unhandled Rejection at: Promise ", p, reason)
+);
+
+server.on("listening", () => console.log("listening on", port));
