d76562bd3535f01d9d5689b59af3c783e5c96b09
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2018-08-25 17:27:31 +0200

    methods

--- src/services/bookmarks.js
+++ src/services/bookmarks.js
@@ -9,19 +9,27 @@ fs.writeFileSync(global.dirname + "/data/1.json", JSON.stringify(data));
 
 class Bookmarks {
   async find(params) {
-    // const s = fs.readFileSync(global.dirname + "/data/1.json", "utf8");
-    // await db.none(`
-    // create table bookmarks (
-    //   id serial primary key
-    //   , name text not null
-    // )
-    // `);
-    // return JSON.parse(s);
-    // return {
-    //   hello: "world"
-    // };
-    await db.manyOrNone("select * from bookmarks");
+    return db.manyOrNone("select * from bookmarks");
   }
+
+  async create(data, params) {
+    return db.one(
+      "insert into bookmarks ($1:name) values ($1:csv) returning *",
+      [data]
+    );
+  }
+
+  async get(id, params) {
+    return db.oneOrNone("select * from bookmarks where id = $1", [id]);
+  }
+
+  async remove(id, params) {
+    return db.oneOrNone("drop from bookmarks where id = $1", [id]);
+  }
+
+  async patch(id, data, params) {}
+
+  async update(id, data, params) {}
 }
 
 module.exports = function(app) {
