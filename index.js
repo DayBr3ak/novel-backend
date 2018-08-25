e1d3f6932bceb5393dc461849116722ed938cc12
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2018-08-25 19:03:13 +0200

    migrate

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 "use strict";
 global.dirname = __dirname;
 
-require("./sql/init.js")()
-  .then(() => {
-    require("./src/index.js");
-  })
-  .catch(console.error);
+// require("./sql/init.js")()
+// .then(() => {
+require("./src/index.js");
+// })
+// .catch(console.error);
