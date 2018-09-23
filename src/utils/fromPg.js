5ada8f4a8a69d2381bb2f3348af5a59e7ef45562
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2018-09-23 20:52:31 +0200

    fix

--- src/utils/fromPg.js
+++ src/utils/fromPg.js
@@ -4,16 +4,6 @@ const { pgp, db } = require("./pg");
 
 class Store {
   constructor() {
-    // this.file = getFile(storeName);
-    // fs.ensureFileSync(this.file);
-
-    // this._dirty = true;
-
-    // try {
-    //   this._content = JSON.parse(fs.readFileSync(this.file));
-    // } catch (e) {
-    //   this._content = {};
-    // }
     this._inited = db
       .none(
         `
@@ -33,10 +23,6 @@ class Store {
       key,
       JSON.stringify(value)
     ]);
-
-    // this._content[key] = value;
-    // await fs.writeFile(this.file, JSON.stringify(this._content));
-    // this._dirty = true;
     return this;
   }
 
@@ -45,6 +31,9 @@ class Store {
     const val = await db.oneOrNone("select value from bks where slug = $1", [
       key
     ]);
+    if (!val) {
+      return undefined;
+    }
     return JSON.parse(val.value);
   }
 
@@ -52,29 +41,12 @@ class Store {
     await this._inited;
     await db.none("delete from bks where slug = $1", [key]);
     return true;
-    // if (this._content[key]) {
-    //   delete this._content[key];
-    //   await fs.writeFile(this.file, JSON.stringify(this._content));
-    //   return true;
-    // }
-    // return false;
   }
 
   async getAll() {
     await this._inited;
     let tmp = await db.manyOrNone("select value from bks");
-    console.log(tmp);
     return tmp.map(x => JSON.parse(x.value)).map(x => [x.slug, x]);
-
-    // let tmp;
-    // if (this._dirty) {
-    //   tmp = Object.keys(this._content).map(x => [x, this._content[x]]);
-    //   this._tmp = tmp;
-    //   this._dirty = false;
-    // } else {
-    //   tmp = this._tmp;
-    // }
-    // return tmp;
   }
 }
 
