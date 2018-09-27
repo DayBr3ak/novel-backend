96e41f7215da5de13a24862072dbeb4df3c57748
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2018-09-27 20:30:00 +0200

    wip

--- sql/bookmarks.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists bookmarks;
-create table bookmarks (
-  id serial primary key
-  , name text not null
-  , reference text not null
-  , chapter text not null
-  , slug text not null
-  , website text not null
-);
--- src/services/index.js
+++
@@ -1,7 +0,0 @@
-const bookmarks = require("./bookmarks");
-const sql = require("./sql");
-
-module.exports = function(app) {
-  app.configure(bookmarks);
-  app.configure(sql);
-};
