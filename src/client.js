a41e71063757100c5c3550deb13a7e0bf2e99459
Benjamin GROENEVELD <benj.groeneveld@gmail.com>
2018-10-13 12:50:10 +0200

    formatting

--- src/client.js
+++ src/client.js
@@ -1,7 +1,7 @@
-import { init } from 'sapper/runtime.js';
-import { manifest } from './manifest/client.js';
+import { init } from "sapper/runtime.js";
+import { manifest } from "./manifest/client.js";
 
 init({
-	target: document.querySelector('#sapper'),
-	manifest
+  target: document.querySelector("#sapper"),
+  manifest
 });
